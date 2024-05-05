412728db7413d4ac4620e01f5f05d8143d0f873e
martyna.radz <mar.rad97@gmail.com>
2024-05-05 08:51:12 +0200

    zad2

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,28 @@ import company.abstracts.Employee;
 import company.models.Manager;
 import company.models.Worker;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Worker worker1 = new Worker("Alicja", 3500.0, 1);
-        Worker worker2 = new Worker("Michał", 3550.0, 2);
-        Worker worker3 = new Worker("Kuba", 3700.0, 3);
-        Worker worker4 = new Worker("Jakub", 3700.0, 4);
+        Worker worker1 = new Worker("Alicja", 3500.0, 1, "2023-01-01", "Junior Developer");
+        Worker worker2 = new Worker("Michał", 3550.0, 2, "2023-02-15", "Junior Developer");
+        Worker worker3 = new Worker("Kuba", 3700.0, 3, "2023-03-20", "Junior Developer");
+        Worker worker4 = new Worker("Jakub", 3700.0, 4, "2023-04-25", "Junior Developer");
 
-        Manager manager = new Manager("Aneta", 5000.0, 5);
+        Manager manager = new Manager("Aneta", 5000.0, 5,  "2022-12-10", "Manager");
 
-        Employee[] employees = {worker1, worker2, worker3, worker4, manager};
+        ArrayList<Employee> employees = new ArrayList<>();
+        employees.add(worker1);
+        employees.add(worker2);
+        employees.add(worker3);
+        employees.add(worker4);
+        employees.add(manager);
 
         for (Employee emp : employees) {
-            System.out.println(emp.getName() + " zarabia " + emp.getSalary());
             emp.work();
+            System.out.println("- " + emp.getName() + " (ID: " + emp.getId() + ", Position: " + emp.getPosition() +
+                    ", Hire date: " + emp.getHireDate() + ", Salary: " + emp.getSalary() + ".");
         }
     }
 }
--- src/company/abstracts/Employee.java
+++ src/company/abstracts/Employee.java
@@ -6,11 +6,15 @@ public abstract class Employee implements Employable {
     private String name;
     private double salary;
     private int id;
+    private String hireDate;
+    private String position;
 
-    public Employee(String name, double salary, int id) {
+    public Employee(String name, double salary, int id, String hireDate, String position) {
         this.name = name;
         this.salary = salary;
         this.id = id;
+        this.hireDate = hireDate;
+        this.position = position;
     }
 
     @Override
@@ -22,5 +26,17 @@ public abstract class Employee implements Employable {
 
     public double getSalary() {
         return salary;
+    }
+
+    public String getHireDate() {
+        return hireDate;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public int getId() {
+        return id;
     }
 }
